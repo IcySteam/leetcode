8f90911783c307ddf49f761391561064dbafa046
IcySteam <IcySteam@users.noreply.github.com>
2021-01-13 01:46:33 +1030

    0 ms

--- solutions/504. Base 7.cpp
+++ solutions/504. Base 7.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    string convertToBase7(int num) {
+    string convertToBase7(int num, int base = 7) {
         if (num == 0) {return "0";}
         stack<int> output_int;
         int abs_num = abs(num);
         while (abs_num > 0) {
-            output_int.push(abs_num % 7);
-            abs_num = abs_num / 7;
+            output_int.push(abs_num % base);
+            abs_num = abs_num / base;
         }
         string output_str = "";
         if (num < 0) {output_str.append("-");}
