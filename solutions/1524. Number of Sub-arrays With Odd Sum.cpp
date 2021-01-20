829802d3f584a0a4b9fc5ec11f7cd4c55980008d
IcySteam <IcySteam@users.noreply.github.com>
2021-01-20 22:22:54 +1030

    N/A

---
+++ solutions/1524. Number of Sub-arrays With Odd Sum.cpp
@@ -0,0 +1,55 @@
+// O(n) solution; too slow
+/*
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int totalNum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            int currentSum = 0;
+            for (int j = i; j < arr.size(); j++) {
+                currentSum += arr[j];
+                if (currentSum % 2 != 0) {totalNum++;}
+            }
+        }
+        return totalNum;
+    }
+};
+*/
+​
+// binary O(n) solution; too slow
+/*
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        vector<int> arrBin;
+        for (auto ele : arr) {arrBin.push_back(ele % 2);}
+        int totalNum = 0; 
+        for (int i = 0; i < arrBin.size(); i++) {
+            bool currentSum = false; // false for odd
+            for (int j = i; j < arrBin.size(); j++) {
+                currentSum = arrBin[j] == 1 ? !currentSum : currentSum;
+                if (currentSum) {totalNum++;}
+            }
+        }
+        return totalNum;
+    }
+};
+*/
+​
+// new solution at O()
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        vector<int> arrBin;
+        for (auto ele : arr) {arrBin.push_back(ele % 2);}
+        int totalNum = 0; 
+        for (int i = 0; i < arrBin.size(); i++) {
+            bool currentSum = false; // false for odd
+            for (int j = i; j < arrBin.size(); j++) {
+                currentSum = arrBin[j] == 1 ? !currentSum : currentSum;
+                if (currentSum) {totalNum++;}
+            }
+        }
+        return totalNum;
+    }
+};
