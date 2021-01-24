59e30406561f426c6f5662917c405b1f9eacf3a2
IcySteam <IcySteam@users.noreply.github.com>
2021-01-25 01:05:40 +1100

    N/A

--- solutions/629. K Inverse Pairs Array.cpp
+++ solutions/629. K Inverse Pairs Array.cpp
@@ -6,33 +6,41 @@ public:
         else {
             vector<int> ascnd;
             for (int i = 1; i <= n; i++) {ascnd.push_back(i);}
-            queue<vector<int>> myQueue;
-            unordered_map<string, bool> existsInQueue;
-            myQueue.push(ascnd);
-            existsInQueue[vecToStr(ascnd)] = true;
+            set<vector<int>> mySet;
+            //unordered_map<string, bool> existsInQueue;
+            mySet.insert(ascnd);
+            //existsInQueue[vecToStr(ascnd)] = true;
             for (int i = 0; i < k; i++) {
-                int lenPre = myQueue.size();
+                int lenPre = mySet.size();
+                set<vector<int>> preSet;
+                for (auto it = mySet.begin(); it != mySet.end(); it++) {
+                    preSet.insert(*it);
+                }
                 for (int j = 0; j < lenPre; j++) {
-                    vector<int> front = myQueue.front();
+                    vector<int> front = *preSet.begin();
                     for (int l = front.size() - 1; l >= 1; l--) {
                         for (int m = l - 1; m >= 0; m--) {
                             if (front[m] < front[l]) {
                                 vector<int> tempArr = front;
                                 swap(tempArr, l, m);
+                                /*
                                 string tempArrStr = vecToStr(tempArr);
                                 if (!existsInQueue[tempArrStr]) {
                                     myQueue.push(tempArr);
                                     existsInQueue[tempArrStr] = true;
-                                }
+                                }
+                                */
+                                mySet.insert(tempArr);
                                 break;
                             }
                         }
                     }
-                    existsInQueue[vecToStr(front)] = false;
-                    myQueue.pop();
+                    //existsInQueue[vecToStr(front)] = false;
+                    mySet.erase(front);
+                    preSet.erase(front);
                 }
             }
-            return myQueue.size();
+            return mySet.size();
         }
     }
     
@@ -42,12 +50,14 @@ public:
         arr[b] = temp;
     }
     
+    /*
     string vecToStr(vector<int>& arr) { // takes O(n), needs improvement; workaround for hash table not supporting vector
         string output = "";
         for (auto& ele : arr) {
             output.append(to_string(ele));
-        }
+        }
         return output;
-    }
+    }
+    */
 };
 ​
