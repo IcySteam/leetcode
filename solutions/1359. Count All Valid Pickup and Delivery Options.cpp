4f8e28db0d88502b5cd99501bd3950ad3ad7f5a6
IcySteam <IcySteam@users.noreply.github.com>
2021-02-01 23:44:14 +1100

    0 ms

--- solutions/1359. Count All Valid Pickup and Delivery Options.cpp
+++ solutions/1359. Count All Valid Pickup and Delivery Options.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int countOrders(int n) {
         long long res = 1, mod = 1e9 + 7;
-        for (int i = 1; i <= n; i++)
-            res = res * (i * 2 - 1) * i % mod;
+        for (int i = n; i >= 1; i--)
+            res = res * i * (i * 2 - 1) % mod;
         return res;
     }
 };
