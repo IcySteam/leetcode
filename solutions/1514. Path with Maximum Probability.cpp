adeac91a2326f36f5ed918700621d4e4e8ae96dc
IcySteam <IcySteam@users.noreply.github.com>
2021-02-10 23:42:42 +1100

    728 ms

--- solutions/1514. Path with Maximum Probability.cpp
+++ solutions/1514. Path with Maximum Probability.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        map<vector<int>, double> probs;
+        map<vector<int>, double> probs; // could use vector of unordered_maps to improve time complexity
         for (int i = 0; i < edges.size(); i++) {
             vector<int> revEdge{edges[i][1], edges[i][0]};
             probs[edges[i]] = succProb[i];
             probs[revEdge] = succProb[i];
         }
-        unordered_map<int, set<int>> graph;
+        unordered_map<int, set<int>> graph; // not necessary actually
         set<int> emptySet;
         for (int i = 0; i < n; i++) {graph[i] = emptySet;}
         for (auto& ele : edges) {
@@ -26,7 +26,7 @@ public:
             for (auto it = graph[front].begin(); it != graph[front].end(); it++) {
                 vector<int> edgeVector{front, *it};
                 double newProb = visitedProbs[front] * probs[edgeVector];
-                if (newProb > visitedProbs[*it]) {
+                if (newProb > visitedProbs[*it]) { // acts as log keeping, IMPORTANT
                     visitedProbs[*it] = newProb;
                     myQueue.push(*it);
                 }
