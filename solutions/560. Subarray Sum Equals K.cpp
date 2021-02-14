b287bcd79cc3e1763c8d20a81997ea610262e491
IcySteam <IcySteam@users.noreply.github.com>
2021-02-15 05:08:02 +1100

    68 ms

--- solutions/560. Subarray Sum Equals K.cpp
+++ solutions/560. Subarray Sum Equals K.cpp
@@ -7,13 +7,14 @@ public:
         int output = 0;
         for (int i = 0; i < nums.size(); i++) {
             currentSum += nums[i];
-            if (myMap.find(currentSum) == myMap.end()) {
-                myMap[currentSum] = 1;
-            }
-            else {currentSum++;}
             if (myMap.find(currentSum - k) != myMap.end()) {
                 output += myMap[currentSum - k];
             }
+            
+            if (myMap.find(currentSum) == myMap.end()) {
+                myMap[currentSum] = 1;
+            }
+            else {myMap[currentSum]++;}
         }
         return output;
     }
