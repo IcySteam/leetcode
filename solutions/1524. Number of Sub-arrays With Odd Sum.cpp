25429d5fc7e7b16545aef7781e99bde6b1f3dd39
IcySteam <IcySteam@users.noreply.github.com>
2021-02-16 02:33:58 +1100

    156 ms

--- solutions/1524. Number of Sub-arrays With Odd Sum.cpp
+++ solutions/1524. Number of Sub-arrays With Odd Sum.cpp
@@ -36,20 +36,39 @@ public:
 };
 */
 ​
-// new solution at O(n) TBD
+// new DP solution at O(n)
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        vector<int> arrBin;
-        for (auto ele : arr) {arrBin.push_back(ele % 2);}
-        int totalNum = 0; 
-        for (int i = 0; i < arrBin.size(); i++) {
-            bool currentSum = false; // false for odd
-            for (int j = i; j < arrBin.size(); j++) {
-                currentSum = arrBin[j] == 1 ? !currentSum : currentSum;
-                if (currentSum) {totalNum++;}
-            }
+        const int BIG_PRIME = 1000000007;
+        if (!arr.size()) {return 0;}
+        vector<int> odds; // can only keep last vals to improve space complexity to O(1)
+        vector<int> evens;
+        if (arr[0] % 2 == 0) {
+            evens.push_back(1);
+            odds.push_back(0);
         }
-        return totalNum;
+        else {
+            evens.push_back(0);
+            odds.push_back(1);
+        }
+        int output = odds[0];
+        for (int i = 1; i < arr.size(); i++) {
+            int lastOdd = odds[i - 1];
+            int lastEven = evens[i - 1];
+            if (arr[i] % 2 == 0) {
+                evens.push_back(lastEven + 1);
+                odds.push_back(lastOdd);
+            }
+            else {
+                evens.push_back(lastOdd);
+                odds.push_back(lastEven + 1);
+            }
+            output = (output + odds[odds.size() - 1]) % BIG_PRIME;
+        }
+        return output;
     }
 };
+​
+    
+​
