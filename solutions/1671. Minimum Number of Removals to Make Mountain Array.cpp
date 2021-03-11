16a4b4f9cebf6bd5c7d6fb7b1ddf90c5d0eab989
IcySteam <IcySteam@users.noreply.github.com>
2021-03-12 02:36:18 +1100

    36 ms

--- solutions/1671. Minimum Number of Removals to Make Mountain Array.cpp
+++ solutions/1671. Minimum Number of Removals to Make Mountain Array.cpp
@@ -1,3 +1,26 @@
+class Solution {
+public:
+    // see 300. Longest Increasing Subsequence
+    vector<int> genLIS(vector<int>& nums) {
+        vector<int> parent(nums.size(), 0); //Tracking the predecessors/parents of elements of each subsequence.
+        vector<int> increasingSub(nums.size() + 1, 0); //Tracking ends of each increasing subsequence.
+        int length = 0; //Length of longest subsequence.
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            //Binary search
+            int low = 1;
+            int high = length;
+            while (low <= high)
+            {
+                int mid = ceil((low + high) / 2);
+                
+                if (nums[increasingSub[mid]] < nums[i]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
             }
             
             int pos = low;
@@ -19,12 +42,30 @@
             LIS[j] = nums[k];
             k = parent[k];
         }
+        
+        cout << "parent: ";
+        for (int i = 0; i < nums.size(); i++) {
+            cout << parent[i] << " ";
+        }
+        cout << endl;
+        cout << "increasingSub: ";
+        for (int i = 0; i < nums.size() + 1; i++) {
+            cout << increasingSub[i] << " ";
+        }
+        cout << endl;
         
         return LIS;
     }
     
     
     int minimumMountainRemovals(vector<int>& nums) {
+        vector<int> lis = genLIS(nums);
+        cout << "LIS: ";
+        for (int i = 0; i < lis.size(); i++) {
+            cout << lis[i] << " ";
+        }
+        cout << endl;
+        
         vector<int> incParentIndexes(nums.size());
         vector<int> incIncreasingSubEndIndexes(nums.size() + 1);
         vector<int> incSubLens(nums.size());
@@ -48,22 +89,3 @@
             decLongestLen = max({decLongestLen, pos});
             decSubLens[i] = pos;
         }
-        
-        //cout << incLongestLen << " " << decLongestLen << endl;
-        int output = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            //cout << incSubLens[i] << " " << decSubLens[i] << endl;
-            if (incSubLens[i] > 1 && decSubLens[i] > 1) {output = max({output, incSubLens[i] + decSubLens[i]});}
-        }
-        output = nums.size() - output + 1;
-        return output;
-    }
-    
-    int myBinSearch(const vector<int>& nums, const vector<int>& indexes, int start, int end, int target) {
-        if (start > end) {return start;}
-        int mid = (start + end) / 2;
-        if (nums[indexes[mid]] < target) {return myBinSearch(nums, indexes, mid + 1, end, target);}
-        else if (nums[indexes[mid]] > target) {return myBinSearch(nums, indexes, start, mid - 1, target);}
-        else {return mid;}
-    }
-};     
