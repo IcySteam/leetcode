77a3335a8561db84eec6098afac576ca6448767c
IcySteam <IcySteam@users.noreply.github.com>
2021-03-19 22:42:39 +1100

    28 ms

--- solutions/3. Longest Substring Without Repeating Characters.cpp
+++ solutions/3. Longest Substring Without Repeating Characters.cpp
@@ -3,17 +3,17 @@ public:
     int lengthOfLongestSubstring(string& s) 
     {
         unordered_set<char> mySet;
-        int lastDuplicate = -1, currentIndex = 0, output = 0;
-        while (lastDuplicate < (int)s.size() && currentIndex < s.size()) { // oof dat auto conversion from signed to unsigned
-            // cout << lastDuplicate << "\t" << currentIndex << endl;
+        int duplicateTracker = -1, currentIndex = 0, output = 0;
+        while (duplicateTracker < (int)s.size() && currentIndex < s.size()) { // oof dat auto conversion from signed to unsigned
+            // cout << duplicateTracker << "\t" << currentIndex << endl;
             if (mySet.find(s[currentIndex]) == mySet.end()) {
                 mySet.insert(s[currentIndex]);
-                output = max({output, currentIndex - lastDuplicate});
+                output = max({output, currentIndex - duplicateTracker});
                 currentIndex++;
             }
             else {
-                lastDuplicate++;
-                mySet.erase(s[lastDuplicate]);
+                duplicateTracker++;
+                mySet.erase(s[duplicateTracker]);
             }
         }
         return output;
