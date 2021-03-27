a49da2ad1dcc3611c5b7c798fb71317acb0c2eef
IcySteam <IcySteam@users.noreply.github.com>
2021-03-28 02:44:29 +1100

    100 ms

--- solutions/322. Coin Change.cpp
+++ solutions/322. Coin Change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    int notMyCoinChange(vector<int>& coins, int amount) {
         vector<int> A(amount + 1, amount + 1); // since smallest coin is 1, biggest potential res is amount
         A[0] = 0; // 0 coin needed to get to amount 0
         for (int coin : coins) {
@@ -9,5 +9,16 @@ public:
             }
         }
         return A[amount] > amount ? -1 : A[amount];
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1); // since smallest coin is 1, biggest potential res is amount
+        dp[0] = 0; // 0 coin needed to get to amount 0
+        for (auto ele : coins) {
+            for (int i = ele; i <= amount; i++) {
+                dp[i] = min({dp[i], dp[i - ele] + 1}); // goes from ele to amount incrementing 1 each time, equivalent to using every amount of each coin denomination (but it's combination, NOT permutation)
+            }
+        }
+        return dp[amount] <= amount? dp[amount] : -1;
     }
 };
