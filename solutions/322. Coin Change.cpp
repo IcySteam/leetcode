e96d75c3bc50a28f238ea76e351714f1a4fa8441
IcySteam <IcySteam@users.noreply.github.com>
2021-03-28 01:31:59 +1100

    N/A

--- solutions/322. Coin Change.cpp
+++ solutions/322. Coin Change.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    unordered_map<int, int> memo;
+    unordered_map<int, unordered_map<int, int>> memo;
     
-    int DFS(vector<int>& coins, int amount, int count) {
-        if (memo.find(amount) != memo.end()) {
-            if (memo[amount] == INT_MAX) {return INT_MAX;}
-            return memo[amount] + count;
+    int DFS(vector<int>& coins, int index, int amount, int count) {
+        if (memo.find(index) != memo.end()) {
+            if (memo[index].find(amount) != memo[index].end()) {
+                if (memo[index][amount] == INT_MAX) {return INT_MAX;}
+                return memo[index][amount] + count;
+            }
         }
-        
+           
         if (amount == 0) {
             return count;
         }
-        else if (amount < 0) {
+        else if (amount < 0 || index >= coins.size()) { // going over / having amount left after running out of denominations
             return INT_MAX;
         }
-        else {
-            vector<int> candidates;
-            for (int i = 0; i < coins.size(); i++) {
-                candidates.push_back(DFS(coins, amount - coins[i], count + 1));
-            }
-            int res = *min_element(candidates.begin(), candidates.end());
-            if (res != INT_MAX) {memo[amount] = res - count;}
-            else {memo[amount] = res;}
-            return res;
+        
+        vector<int> candidates;
+        for (int i = 0; i < (amount / coins[index]) + 1; i++) { // +1 important!!!
+            int res_tmp = DFS(coins, index + 1, amount - coins[index] * i, count + i);
+            // cout << res_tmp << endl;
+            candidates.push_back(res_tmp);
         }
+        if (candidates.empty()) {
+            memo[index][amount] = INT_MAX;
+            return INT_MAX;
+        }
+        
+        int res = *min_element(candidates.begin(), candidates.end());
+        if (res != INT_MAX) {memo[index][amount] = res - count;}
+        else {memo[index][amount] = res;}
+        return res;
     }
     
     int coinChange(vector<int>& coins, int amount) {
-        int output = DFS(coins, amount, 0);
+        int output = DFS(coins, 0, amount, 0);
         if (output == INT_MAX) {output = -1;}
         return output;
     }
